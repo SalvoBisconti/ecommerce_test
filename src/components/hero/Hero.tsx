8d53687b7434101290812121a940c5c88eddb444
Salvatore Bisconti <121030864+SalvoBisconti@users.noreply.github.com>
2024-03-11 22:39:28 +0100

    fixed

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "../button";
 const Hero = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center relative bg-parallax bg-fixed bg-cover">
-      <div className="absolute top-0 w-full h-full z-20 bg-[#0000004d]"></div>
+      <div className="absolute top-0 w-screen h-full z-20 bg-[#0000004d]"></div>
       <div className="w-[90vw] md:w-[50vw] flex flex-col justify-center items-center gap-3 z-30">
         <h3 className="text-mobileTitle md:text-deskTitle  lg:text-strongDeskTitle  font-bold text-white">
           Welcome to{" "}
