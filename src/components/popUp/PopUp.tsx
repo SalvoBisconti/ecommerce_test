72bf5baf4d35efd5d41ebe193288cdffc96c0e8d
Salvatore Bisconti <121030864+SalvoBisconti@users.noreply.github.com>
2024-03-25 17:40:02 +0100

    fix

--- src/components/popUp/PopUp.tsx
+++ src/components/popUp/PopUp.tsx
@@ -3,7 +3,7 @@ const PopUp = (props: { isProductAdded: boolean }) => {
   return (
     <div
       className={` ${
-        isProductAdded ? "top-16" : "-top-20"
+        isProductAdded ? "top-20" : "-top-20"
       } px-4 py-2 fixed  bg-secondColor text-white rounded-xl  z-50 transition-all duration-500`}
     >
       <h2>Added to cart</h2>
